1eeca58e90dfcc16fc9e9e033c207696bd9b1734
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2023-03-12 16:16:40 +0000

    search data with params url

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import './index.css'
 
-const Search = ({ search, setSearch, fetchQuestions }) => {
+const Search = ({ handleSearch, handleInputChange, handleClearClick, filter }) => {
     return (
         <div className='search'>
-            <input type='text' placeholder='Search by keyword' value={search} onChange={(e) => setSearch(e.target.value)} />
-            <div className='actions'>
-                {
-                    search.length > 2 &&
-                    <button className='button' onClick={() => fetchQuestions()}>Search</button>
-                }
-                <button className='button' onClick={() => {
-                        setSearch('')
-                        fetchQuestions()
-                    }}>Cancel</button>
-            </div>
+            <form onSubmit={handleSearch}>
+                <input type="text" placeholder="Search" value={filter} onChange={handleInputChange}/>
+                <div className='actions'>
+                    <button className='button' type="submit">Search</button>
+                    <button className='button' onClick={handleClearClick}>Clear</button>
+                </div>
+            </form>
         </div>
+        // <div className='search'>
+        //     <input type='text' placeholder='Search by keyword' value={search} onChange={(e) => setSearch(e.target.value)} />
+        //     <div className='actions'>
+        //         {
+        //             search.length > 2 &&
+        //             <button className='button' onClick={() => fetchQuestions()}>Search</button>
+        //         }
+        //         <button className='button' onClick={() => {
+        //                 setSearch('')
+        //                 fetchQuestions()
+        //             }}>Cancel</button>
+        //     </div>
+        // </div>
     )
 }
 
--- src/screens/Questions/index.jsx
+++ src/screens/Questions/index.jsx
@@ -1,74 +1,132 @@
-import './index.css'
-import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import './index.css';
+import { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import queryString from 'query-string';
 
 // api
-import api from '../../services/api'
+import api from '../../services/api';
 
 // components
-import Header from '../../components/Header'
-import Loading from '../../components/Loading'
-import Search from '../../components/Search'
-import QuestionsList from '../../components/QuestionsList'
-import Pagination from '../../components/Pagination'
+import Header from '../../components/Header';
+import Loading from '../../components/Loading';
+import Search from '../../components/Search';
+import QuestionsList from '../../components/QuestionsList';
+import Pagination from '../../components/Pagination';
 
 const Questions = () => {
-
   let navigate = useNavigate();
+  const location = useLocation();
 
-  const [questions, setQuestions] = useState([])
-  const [total, setTotal] = useState(0)
-  const [search, setSearch] = useState('')
-  const [loading, setLoading] = useState(true)
-  const [offset, setOffset] = useState(0)
-  const [limit, setLimit] = useState(10)
+  const [filter, setFilter] = useState('');
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(10);
 
   async function fetchQuestions() {
-    // GET /questions?limit={limit}&offset={offset}&filter={filter} 
-    if (search === '') {
-      const response = await api.get(`/questions?limit=${limit}&offset=${offset}`)
-      setTotal(response.data.length)
-      setQuestions(response.data)
-      setLoading(false)
-      return
+    try {
+      let response;
+      if (filter) {
+        response = await api.get(`/questions?limit=${limit}&offset=${offset}&filter=${filter}`);
+      } else {
+        response = await api.get(`/questions?limit=${limit}&offset=${offset}`);
+      }
+      setLoading(false);
+      setQuestions(response.data);
+    } catch (error) {
+      console.error(error);
     }
-    const response = await api.get(`/questions?limit=${limit}&offset=${offset}&filter=${search}`)
-    setTotal(response.data.length)
-    setQuestions(response.data)
-    setSearch('')
-    setLoading(false)
+  }
+
+  function handleInputChange(e) {
+    setFilter(e.target.value);
+  }
+
+  function handleClearClick() {
+    setFilter('');
+    const queryParams = queryString.parse(location.search);
+    delete queryParams.filter;
+    const newQueryParams = queryString.stringify(queryParams);
+    window.history.pushState({}, '', `/questions?${newQueryParams}`);
   }
 
   function handleNavigateToQuestion(id) {
-    navigate(`/questions/${id}`)
+    navigate(`/questions/${id}`);
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const queryParams = queryString.parse(location.search);
+    queryParams.filter = filter;
+    const newQueryParams = queryString.stringify(queryParams);
+    window.history.pushState({}, '', `/questions?${newQueryParams}`);
+    fetchQuestions();
   }
 
   useEffect(() => {
-    fetchQuestions()
-  }, [offset])
+    const queryParams = queryString.parse(location.search);
+    const filterParam = queryParams.filter;
+    setFilter(filterParam || '');
+  }, [location]);
+
+  useEffect(() => {
+    async function fetchQuestionsWithFilter() {
+      try {
+        let response = await api.get(`/questions?limit=${limit}&offset=${offset}&filter=${filter}`);
+        setLoading(false);
+        setQuestions(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async function fetchQuestionsWithoutFilter() {
+      try {
+        let response = await api.get(`/questions?limit=${limit}&offset=${offset}`);
+        setLoading(false);
+        setQuestions(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    if (filter) {
+      fetchQuestionsWithFilter();
+    } else {
+      fetchQuestionsWithoutFilter();
+    }
+  }, [offset, limit, filter, location.search]);
 
   return (
     <>
       <Header />
       <div className='container'>
-        {
-          loading ?
-            <Loading />
-            :
-            <>
-              {/* search component */}
-              <Search search={search} setSearch={setSearch} fetchQuestions={fetchQuestions} />
-
-              {/* questions list component */}
-              <QuestionsList questions={questions} handleNavigateToQuestion={handleNavigateToQuestion} />
-
-              {/* pagination component */}
-              <Pagination offset={offset} setOffset={setOffset} limit={limit} setLimit={setLimit} total={total} />
-            </>
-        }
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <Search
+              handleSearch={handleSearch}
+              handleInputChange={handleInputChange}
+              handleClearClick={handleClearClick}
+              filter={filter}
+            />
+            <QuestionsList
+              questions={questions}
+              handleNavigateToQuestion={handleNavigateToQuestion}
+            />
+            <Pagination
+              offset={offset}
+              setOffset={setOffset}
+              limit={limit}
+              setLimit={setLimit}
+              total={questions.length}
+            />
+          </>
+        )}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Questions
