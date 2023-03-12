c040bd7d21dff9d0e7c387ea51da5f1bbb988383
Marcelo Rodrigues Alves <marceloalves_ac@hotmail.com>
2023-03-12 16:30:42 +0000

    filter.length

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -7,24 +7,14 @@ const Search = ({ handleSearch, handleInputChange, handleClearClick, filter }) =
             <form onSubmit={handleSearch}>
                 <input type="text" placeholder="Search" value={filter} onChange={handleInputChange}/>
                 <div className='actions'>
-                    <button className='button' type="submit">Search</button>
+                    {
+                        filter.length > 2 &&
+                        <button className='button' type="submit">Search</button>
+                    }
                     <button className='button' onClick={handleClearClick}>Clear</button>
                 </div>
             </form>
         </div>
-        // <div className='search'>
-        //     <input type='text' placeholder='Search by keyword' value={search} onChange={(e) => setSearch(e.target.value)} />
-        //     <div className='actions'>
-        //         {
-        //             search.length > 2 &&
-        //             <button className='button' onClick={() => fetchQuestions()}>Search</button>
-        //         }
-        //         <button className='button' onClick={() => {
-        //                 setSearch('')
-        //                 fetchQuestions()
-        //             }}>Cancel</button>
-        //     </div>
-        // </div>
     )
 }
 
